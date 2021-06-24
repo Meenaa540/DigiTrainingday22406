b077e7243281899431b298ddac8732f7f8ab9f81
Meenaa540 <86398344+Meenaa540@users.noreply.github.com>
2021-06-24 20:50:27 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DigiTrainingClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
